b247352af2602d23baf32c6b39596657e587fb1d
awakenpruflass <160042156+awakenpruflass@users.noreply.github.com>
2024-02-14 20:46:57 +0400

    fweb p1

--- src/header.jsx
+++ src/header.jsx
@@ -1,36 +1,51 @@
-import qavtasi from "./qavtaslogo.png"
-import qavtlang from "./qavtaslang.png"
-import qavtshek from "./qavtshek.png"
-import qavtmaz1 from "./qavtmaz1.png"
+import qavtasi from "./qavtaslogo.png";
+import qavtlang from "./qavtaslang.png";
+import qavtshek from "./qavtshek.png";
+import qavtmaz1 from "./qavtmaz1.png";
+import qavtmaz2 from "./qavtmaz2.png";
 
+export default function Header() {
+  return (
+    <div>
+      <div className="zevit">
+        <p className="nom">548764238</p>
+      </div>
+      <div className="qavtasi">
+        <img src={qavtasi} />
 
-export default function Header(){
-        return (
-           <div>
-                <div className="zevit">
-                    <p className="nom">548764238</p>
-                </div>
-                <div className="qavtasi">
-                <img src={qavtasi}  />
+        <p>მთავარი</p>
+        <p>პროდუქცია</p>
+        <p>ბლოგი</p>
+        <p>ჩვენს შესახებ</p>
+        <p>კონტაქტები</p>
 
-                <p>მთავარი</p>
-                <p>პროდუქცია</p>
-                <p>ბლოგი</p>
-                <p>ჩვენს შესახებ</p>
-                <p>კონტაქტები</p>
+        <img src={qavtlang} className="ena" />
+      </div>
 
-                <img src={qavtlang} className="ena" />
-            </div>
+      <div className="mt">
+        <div className="mtavari">
+          <img src={qavtshek} className="shek" />
+          <div>
+            <img src={qavtmaz1} className="maz1" />
+            <img src={qavtmaz2} className="maz2" />
+          </div>
+        </div>
+      </div>
 
-            
-            <img src={qavtshek} className="shek" />
-                <div>
-
-                <img src={qavtmaz1} className="maz1" />
-
-                </div>
-
-
-           </div>
-        )
+      <div className="mtavari2">
+        <div>
+          <img src={qavtshek} className="shek2" />
+          <p className="zz">ნაყენი</p>
+        </div>
+        <div>
+          <img src={qavtmaz1} className="shek2" />
+          <p className="zz">მალამო</p>
+        </div>
+        <div>
+          <img src={qavtmaz2} className="shek2" />
+          <p className="zz">მალამო</p>
+        </div>
+      </div>
+    </div>
+  );
 }
